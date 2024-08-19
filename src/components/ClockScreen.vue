3569ebc455450903939ef919ef8976b4b948843f
ひろ <koki.hiro0723@gmail.com>
2024-08-20 02:39:28 +0900

    アプリに時間を表示

---
+++ src/components/ClockScreen.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div class="clock">
+      <div class="date">
+        <p>{{ year }}/{{ month }}/{{ day }}</p>
+      </div>
+      <div class="time">
+        <p>
+          {{ hour }}:{{ minute }}<span class="seconds">:{{ second }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "ClockScreen",
+  data() {
+    return {
+      // 現在の日時を保持
+      date: new Date(),
+    };
+  },
+  computed: {
+    // 年
+    year() {
+      return this.date.getFullYear();
+    },
+    // 月
+    month() {
+      return this.date.getMonth() + 1;
+    },
+    // 日
+    day() {
+      return this.dateTimePadding(this.date.getDate());
+    },
+    // 時
+    hour() {
+      return this.dateTimePadding(this.date.getHours());
+    },
+    // 分
+    minute() {
+      return this.dateTimePadding(this.date.getMinutes());
+    },
+    // 秒
+    second() {
+      return this.dateTimePadding(this.date.getSeconds());
+    },
+  },
+  mounted() {
+    // 現在日時をセット
+    this.setDate();
+    // 1秒ごとに現在日時を更新
+    setInterval(() => {
+      this.setDate();
+    }, 1000);
+  },
+  methods: {
+    // 日時を2桁に変換
+    dateTimePadding(num: number) {
+      return ("0" + num).slice(-2);
+    },
+
+    // 現在日時をセット
+    setDate() {
+      this.date = new Date();
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* スタイルはここに追加 */
+</style>
